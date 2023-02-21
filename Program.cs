7d6727e689edcc2d116ff02365a65c500a1e9491
Andrei Aliaksandrau <aleksandrov.a.95@yandex.ru>
2023-02-21 15:39:11 +0300

    array creation method added

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string[] textInitial = GetStringArrayFromUser(); // создание массива из строк
+
+//string[] textFinal = GetTextLessThanThree(textInitial); // определяем элементы размером менее 3 символов и записвываем в новый массив
+
+//PrintArrays(textInitial, textFinal); // выводим в консоль первоначальный массив и конечный
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+string[] GetStringArrayFromUser() 
+{
+    Console.Write("Введите длину массива: ");
+    int length = int.Parse(Console.ReadLine() ?? "");
+
+    Console.WriteLine("Введите содержимое массива:");
+    string[] stringArray = new string[length];
+    for (int i = 0; i < length; i++) 
+    {
+        stringArray[i] = Console.ReadLine() ?? "";
+    }
+
+    return stringArray;
+}
